f42997765ffbd60ef2e341b8bd1e86e926de40ac
Shubham Agrawal <agrshubh@amazon.com>
2022-03-04 13:02:40 -0800

    Adding ResourceNotFound error code

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='custom_connector_sdk',
-      version='0.1',
-      description='',
-      url='',
-      author='',
-      author_email='',
-      license='',
+      version='1.0',
+      description='Amazon AppFlow Custom Connector SDK',
+      url='https://github.com/awslabs/aws-appflow-custom-connector-python',
+      author='Amazon AppFlow',
+      license="Apache License 2.0",
+      python_requires=">= 3.6",
       packages=['custom_connector_sdk'],
-      zip_safe=False)
+      zip_safe=False
+      )
