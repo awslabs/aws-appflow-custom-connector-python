4f2f06884612b22f987823d42468a4511ecec0aa
Chaitanya Gohel <104654647+gashutos@users.noreply.github.com>
2022-08-08 16:32:45 -0700

    Basic auth support for OAuth2
    https://github.com/awslabs/aws-appflow-custom-connector-java/pull/30/files

--- custom_connector_sdk/connector/auth.py
+++ custom_connector_sdk/connector/auth.py
@@ -26,6 +26,7 @@ AUTHENTICATION_TYPE = 'authenticationType'
 CUSTOM_CREDENTIALS = 'customCredentials'
 AUTH_PARAMETERS = 'authParameters'
 O_AUTH_2_CUSTOM_PROPERTIES = 'oAuth2CustomProperties'
+ADD_BASIC_AUTH_HEADER = 'addBasicAuthHeader'
 BASIC_AUTH_CREDENTIALS = 'basicAuthCredentials'
 API_KEY_CREDENTIALS = 'apiKeyCredentials'
 O_AUTH_2_CREDENTIALS = 'oAuth2Credentials'
@@ -204,6 +205,7 @@ class OAuth2Defaults:
                  o_auth_2_grant_types_supported: List[OAuth2GrantType],
                  o_auth_scopes: List[str] = None,
                  o_auth_2_custom_parameters: List[Oauth2CustomParameter] = None,
+				 add_basic_auth_header: bool,
                  o_auth_2_content_type: OAuth2ContentType = OAuth2ContentType.URL_ENCODED,
                  o_auth_2_method_type: OAuth2MethodType = OAuth2MethodType.HTTP_POST):
         # OAuth Scopes.
@@ -234,6 +236,9 @@ class OAuth2Defaults:
         # scope defined as the default parameter for AUTH_URL to generate the Authorization code.
         # Connector developer doesn't have to define clientId and scope as OAuth2Custom Parameter for AUTH_URL.
         self.o_auth_2_custom_parameters = o_auth_2_custom_parameters
+		
+		# Specifies if clientId and clientSecret should be added to basic auth header
+		self.add_basic_auth_header = add_basic_auth_header
 
         # OAuth2 content type used for login requests. URL_ENCODED or APPLICATION_JSON.
         # Default: URL_ENCODED
@@ -250,6 +255,7 @@ class OAuth2Defaults:
                 O_AUTH_2_GRANT_TYPES_SUPPORTED: [grant_type.name for grant_type in self.o_auth_2_grant_types_supported],
                 O_AUTH_SCOPES: self.o_auth_scopes,
                 O_AUTH_2_CUSTOM_PROPERTIES: self.o_auth_2_custom_parameters and [param.to_dict() for param in self.o_auth_2_custom_parameters],
+				ADD_BASIC_AUTH_HEADER: self.add_basic_auth_header and [param.to_dict() for param in self.add_basic_auth_header],
                 O_AUTH_2_CONTENT_TYPE: self.o_auth_2_content_type.name,
                 O_AUTH_2_METHOD_TYPE: self.o_auth_2_method_type.name}
 
